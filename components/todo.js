78f30d5a3ecba9b075829e504eba63dc3dd2fe28
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-07-10 07:01:24 +0800

    attempt to implement modal box for editting
    following tutorial from: https://www.w3schools.com/howto/howto_css_modals.asp

--- components/todo.js
+++ components/todo.js
@@ -57,7 +57,8 @@ function editItem(){
 	var currentData = document.querySelector('.edTask:checked').data;
 	// console.log(index);
 	// var editValue = document.getElementById("editField").value;  //TODO: abstract
-	var editValue = prompt("enter new data: ")
+	// var editValue = prompt("enter new data: ");  // prompt
+	document.getElementById("modal").style.display = "block";
 
 	var array = unjsonify(localStorage.getItem(localKey));
 	array.splice(index, 1, {
@@ -99,7 +100,7 @@ function display(){
 			// + "<span onclick='editItem()' class='edTask' id='" + i + "'" + ">  Edit </span>" // controls the delete "button"
 			// + "<input type='radio' class='edTask id='" + i + "'" + ">"
 			+ "<label class='edit' onclick='editItem()'>" 
-			+ "<input type='checkbox'"
+			+ "<input type='checkbox' " 
 			+ "class='edTask'"
 			+ "id ='" + i + "'"
 			+ "> edit "	
@@ -108,4 +109,32 @@ function display(){
 		);
 
 	}
+}
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
