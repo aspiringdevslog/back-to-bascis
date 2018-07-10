0f8ea206ee88ac5b5e5a2e029b500c50f6cccc31
AspiringDevsLog <40519130+aspiringdevslog@users.noreply.github.com>
2018-07-10 08:47:58 +0800

    modal implementation
    - made use of css display
    - use javascript to change display between block and none

--- components/todo.js
+++ components/todo.js
@@ -53,12 +53,18 @@ function deleteItem(){
 }
 
 function editItem(){
-	var index = document.querySelector('.edTask:checked').id; //  //TODO: abstract
+	// console.log(index);
+	if(document.querySelector('.edTask:checked').id){
+		var index = document.querySelector('.edTask:checked').id; //  //TODO: abstract
+	} else {
+		var index = document.querySelector('.edTask:checked').id; //  //TODO: abstract
+	}
+
 	var currentData = document.querySelector('.edTask:checked').data;
 	// console.log(index);
 	// var editValue = document.getElementById("editField").value;  //TODO: abstract
-	// var editValue = prompt("enter new data: ");  // prompt
-	document.getElementById("modal").style.display = "block";
+	var editValue = prompt("enter new data: ");  // prompt
+	// document.getElementById("modal").style.display = "block";
 
 	var array = unjsonify(localStorage.getItem(localKey));
 	array.splice(index, 1, {
@@ -111,30 +117,12 @@ function display(){
 	}
 }
 
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+function modalClick(){
+	var x = document.getElementsByClassName('modal');
+	x[0].style.display="block"; 
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function modalClose(){
+	var x = document.getElementsByClassName('modal');
+	x[0].style.display="none"; 
 }
